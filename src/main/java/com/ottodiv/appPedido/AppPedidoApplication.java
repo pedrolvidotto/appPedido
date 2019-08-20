6890a97b6731d6f4ac434eb19d154b567be6d4c0
pedrol.vidotto@gmail.com <pedrol.vidotto@gmail.com>
2019-08-20 08:20:15 -0300

    criando operacao de instanciacao

--- src/main/java/com/ottodiv/appPedido/AppPedidoApplication.java
+++ src/main/java/com/ottodiv/appPedido/AppPedidoApplication.java
@@ -1,13 +1,34 @@
 package com.ottodiv.appPedido;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AppPedidoApplication {
+import com.ottodiv.appPedido.domain.Categoria;
+import com.ottodiv.appPedido.repositories.CaregoriaRepository;
+import com.ottodiv.appPedido.resources.CategoriaResource;
 
+@SpringBootApplication
+public class AppPedidoApplication implements CommandLineRunner{
+
+	@Autowired
+	private CaregoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AppPedidoApplication.class, args);
 	}
 
+	
+	@Override
+	public void run(String...arg) throws Exception{
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2) );
+	}
 }
