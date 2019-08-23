3d9e1d8fd1b2d903c2ef6d13289b1382bc4dc0cc
pedrol.vidotto@gmail.com <pedrol.vidotto@gmail.com>
2019-08-22 22:01:55 -0300

    Produto e associcao muitos para muitos

--- src/main/java/com/ottodiv/appPedido/AppPedidoApplication.java
+++ src/main/java/com/ottodiv/appPedido/AppPedidoApplication.java
@@ -10,7 +10,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ottodiv.appPedido.domain.Categoria;
+import com.ottodiv.appPedido.domain.Produto;
 import com.ottodiv.appPedido.repositories.CaregoriaRepository;
+import com.ottodiv.appPedido.repositories.ProdutoRepository;
 import com.ottodiv.appPedido.resources.CategoriaResource;
 
 @SpringBootApplication
@@ -19,6 +21,9 @@ public class AppPedidoApplication implements CommandLineRunner{
 	@Autowired
 	private CaregoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AppPedidoApplication.class, args);
 	}
@@ -28,7 +33,19 @@ public class AppPedidoApplication implements CommandLineRunner{
 	public void run(String...arg) throws Exception{
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritório");
+	
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impresora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2) );
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 }
